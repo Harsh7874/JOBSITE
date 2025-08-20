e37fcacbac87f3d9e94f534ab7f9381e1c50e361
Harsh7874 <142920362+Harsh7874@users.noreply.github.com>
2025-08-20 17:39:46 +0530

    Create job.js

---
+++ job.js
@@ -0,0 +1,59 @@
+
+    "jobs": [
+        {
+            "_id": "68a5a8f983376f80004de85a",
+            "jobtitle": "Frontend Developer",
+            "companyName": "TechNova Solutions",
+            "pay": 55000,
+            "experience": 1
+        },
+        {
+            "_id": "68a5a8f983376f80004de85b",
+            "jobtitle": "Backend Engineer",
+            "companyName": "CodeWave Labs",
+            "pay": 75000,
+            "experience": 3
+        },
+        {
+            "_id": "68a5a8f983376f80004de85c",
+            "jobtitle": "Full Stack Developer",
+            "companyName": "InnoSoft Pvt Ltd",
+            "pay": 90000,
+            "experience": 4
+        },
+        {
+            "_id": "68a5a8f983376f80004de85d",
+            "jobtitle": "Data Analyst",
+            "companyName": "InsightWorks",
+            "pay": 65000,
+            "experience": 2
+        },
+        {
+            "_id": "68a5a8f983376f80004de85e",
+            "jobtitle": "DevOps Engineer",
+            "companyName": "Cloudify Inc.",
+            "pay": 80000,
+            "experience": 5
+        },
+        {
+            "_id": "68a5a8f983376f80004de85f",
+            "jobtitle": "Software Tester",
+            "companyName": "BugHunt Technologies",
+            "pay": 50000,
+            "experience": 0
+        },
+        {
+            "_id": "68a5a8f983376f80004de860",
+            "jobtitle": "Machine Learning Engineer",
+            "companyName": "AIverse",
+            "pay": 95000,
+            "experience": 3
+        },
+        {
+            "_id": "68a5a8f983376f80004de861",
+            "jobtitle": "UI/UX Designer",
+            "companyName": "Designly Studio",
+            "pay": 60000,
+            "experience": 2
+        }
+    ]
